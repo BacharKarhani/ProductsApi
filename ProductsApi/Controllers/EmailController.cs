76650cfd03b434ca20e5a3af0574f9af41368144
BacharKarhani <132472406+BacharKarhani@users.noreply.github.com>
2025-01-15 12:54:47 +0200

    Update EmailController.cs

--- ProductsApi/Controllers/EmailController.cs
+++ ProductsApi/Controllers/EmailController.cs
@@ -38,6 +38,8 @@ public class EmailController : ControllerBase
     public IActionResult GetHello()
     {
         return Ok(new { message = "Hello!" });
+                        Console.WriteLine("Hello");
+
 
     }
 
